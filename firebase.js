6f3f2177e0f584097e043b7ef672ec5e0e9bdcd1
Coolzzzer <andrew.zhuk936@gmail.com>
2025-06-12 22:27:35 +0300

    update json

--- firebase.js
+++ firebase.js
@@ -1,8 +1,8 @@
 const admin = require("firebase-admin")
-const serviceAccount = require("./twilightimperiumhelperdatabase-firebase-adminsdk-fbsvc-c7044a3efe.json")
+const serviceAccount = require("./twilightimperiumhelperdatabase-firebase-adminsdk-fbsvc-d8e999b7e4.json")
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 const db = admin.firestore();
-module.exports = db;
+module.exports = db
